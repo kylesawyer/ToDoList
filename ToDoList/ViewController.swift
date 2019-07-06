10eafb21e1f7c06e175f48da100a13d02b553925
Kyle <kylemmsawyer@gmail.com>
2019-07-06 11:22:34 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Kyle Sawyer on 2019-07-06.
+//  Copyright © 2019 Kyle Sawyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
